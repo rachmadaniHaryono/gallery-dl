2cced65683e2eb00fc96096980ba9178fe3f8665
rachmadaniHaryono <foreturiga@gmail.com>
2017-08-04 18:42:47 +0800

    chg: dev: add models

--- gallery_dl/server.py
+++ gallery_dl/server.py
@@ -2,11 +2,14 @@
 # -*- coding: utf-8 -*-
 import json
 
+from flask import Flask, render_template, request
+import peewee
+
 from .job import DataJob
 from .exception import NoExtractorError
+from . import models
 
 
-from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -27,14 +30,30 @@ class CustomDataJob(DataJob):
             self.data.append((exc.__class__.__name__, str(exc)))
         return self.data
 
+
+def get_or_create_gallery(url_input):
+    """get or create gallery."""
+    models.db.init('gallery_dl.db')
+    models.Gallery.create_table(True)
+    entries = [
+        x.to_dataset()
+        for x in models.Gallery.select().where(models.Gallery.src_url == url_input)
+    ]
+    if entries:
+        return entries
+    job = CustomDataJob(url_input)
+    entries = job.get_data()[2:]
+    list(map(lambda x: models.Gallery.save_from_dataset(x, url_input), entries))
+    return entries
+
+
 @app.route('/')
 def index():
     url_input = request.args.get('url', None)
     if url_input is None:
         return render_template('index.html')
     try:
-        job = CustomDataJob(url_input)
-        entries = job.get_data()[2:]
+        entries = get_or_create_gallery(url_input)
         return render_template('index.html', entries=entries, url_input=url_input)
     except NoExtractorError as e:
         return render_template('index.html', error="No Extractor found.", url_input=url_input)

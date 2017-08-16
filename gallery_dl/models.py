45a68c08a08711730b8abce81a54a9a8b1379964
rachmadaniHaryono <foreturiga@gmail.com>
2017-08-16 17:59:58 +0800

    chg: dev: update to v2
    - update model
    - update server and server's route
    - update gallery html.

--- gallery_dl/models.py
+++ gallery_dl/models.py
@@ -1,51 +1,44 @@
-from peewee import *
+"""Model module."""
+from peewee import (
+    SqliteDatabase,
+    ForeignKeyField,
+    Model,
+    CharField,
+)
+from playhouse.kv import JSONField
 
 db = SqliteDatabase(None)
 
-class Gallery(Model):
-    src_url = CharField()
-    index = IntegerField()
-    url = CharField()
-    # from info dict
-    artist = CharField(null=True)
-    count = CharField(null=True)
-    extension = CharField(null=True)
-    image_id = CharField(null=True)
-    lang = CharField(null=True)
-    language = CharField(null=True)
-    name = CharField(null=True)
-    num = IntegerField(null=True)
-    section = CharField(null=True)
-    tags = CharField(null=True)
-    title = CharField(null=True)
 
-    def to_dataset(self):
-        return [
-            self.index, self.url,
-            {
-                'artist': self.artist,
-                'count': self.count,
-                'extension': self.extension,
-                'image-id': self.image_id,
-                'lang': self.lang,
-                'language': self.language,
-                'name': self.name,
-                'num': self.num,
-                'section': self.section,
-                'tags': self.tags,
-                'title': self.title,
-            }
-        ]
-
-    @staticmethod
-    def save_from_dataset(dataset, src_url):
-        dataset_info = dataset[2]
-        image_id = dataset_info.pop('image-id', None)
-        obj = Gallery(
-            src_url=src_url, index=dataset[0], url=dataset[1], imagee_id=image_id, **dataset_info)
-        obj.save()
-        return obj
-
+class BaseModel(Model):
+    """Base model."""
 
     class Meta:
-        database = db # This model uses the "people.db" database.
+        database = db
+
+
+class Url(BaseModel):
+    """Url Model."""
+
+    value = CharField(unique=True)
+
+
+class Gallery(BaseModel):
+    """Gallery."""
+
+    url = ForeignKeyField(Url, unique=True)
+    metadata = JSONField(null=True)
+
+
+class Post(BaseModel):
+    """Post."""
+
+    gallery = ForeignKeyField(Gallery)
+    url = ForeignKeyField(Url)
+
+
+class PostMetadata(BaseModel):
+    """Post."""
+
+    post = ForeignKeyField(Post)
+    metadata = JSONField(null=True)
--- gallery_dl/server.py
+++ gallery_dl/server.py
@@ -1,77 +1,157 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import json
-
+"""Server module."""
 try:
-    from flask import Flask, render_template, request
-    import peewee
+    from flask import (
+        flash,
+        Flask,
+        redirect, url_for,
+        render_template,
+        request,
+    )
+    import peewee  # NOQA
 except ImportError as e:
     print("Peewee and flask package required for server feature.")
     raise e
 
-from .job import DataJob
+from .job import Job
 from .exception import NoExtractorError
 from . import models
-
-
+from .models import (
+    Post,
+    PostMetadata,
+    Gallery,
+    Url,
+)
 
 
 app = Flask(__name__)
 
 
-class CustomDataJob(DataJob):
+class CustomJob(Job):
+    """Custom job."""
 
-    def get_data(self):
-        """get data."""
-        try:
-            for msg in self.extractor:
-                copy = [
-                    part.copy() if hasattr(part, "copy") else part
-                    for part in msg
-                ]
-                self.data.append(copy)
-        except Exception as exc:
-            self.data.append((exc.__class__.__name__, str(exc)))
-        return self.data
+    def __init__(self, url):
+        """Init method."""
+        Job.__init__(self, url)
+        self.gallery_data = {}
+        self.images = []
+
+    def handle_url(self, url, metadata):
+        """Handle url."""
+        self.images.append((url, metadata.copy()))
+
+    def handle_directory(self, metadata):
+        """handle directory."""
+        self.gallery_data = metadata.copy()
 
 
-def get_or_create_gallery(url_input):
-    """get or create gallery."""
+def init_db():
+    """Init db."""
     models.db.init('gallery_dl.db')
-    models.Gallery.create_table(True)
-    entries = [
-        x.to_dataset()
-        for x in models.Gallery.select().where(models.Gallery.src_url == url_input)
-    ]
-    if entries:
-        return entries
-    job = CustomDataJob(url_input)
-    entries = job.get_data()[2:]
-    list(map(lambda x: models.Gallery.save_from_dataset(x, url_input), entries))
-    return entries
+    models.db.create_tables([
+        Gallery,
+        Post,
+        PostMetadata,
+        Url,
+    ], safe=True)
+
+
+def get_or_create_posts(url_input):
+    """Get or create gallery."""
+    url_input_m, _ = Url.get_or_create(url_input)
+    gallery_m, _ = Gallery.get_or_create(url=url_input_m)
+    posts_q = Post.select().where(Post.gallery == gallery_m)
+    if posts_q.exists():
+        for entry in posts_q:
+            yield entry, False
+    job = CustomJob(url_input_m.value)
+    job.run()
+    gallery_m.metadata.update(job.gallery_data)
+    gallery_m.save()
+    for img_url , img_metadata in job.images:
+        post_url_m, _ = Url.get_or_create(img_url)
+        post_m, _ = Post.get_or_create(gallery=gallery_m, url=post_url_m)
+        post_metadata_m, is_created = PostMetadata.get_or_create(post=post_m)
+        post_metadata_m.metadata.update(img_metadata)
+        post_metadata_m.save()
+        yield post_metadata_m, is_created
 
 
 @app.route('/')
 def index():
+    """Get index page."""
     url_input = request.args.get('url', None)
     if url_input is None:
         return render_template('index.html')
+    url_input_m, _ = Url.get_or_create(value=url_input)
+    gallery_m, _ = Gallery.get_or_create(url=url_input_m)
+    return redirect(url_for('gallery', gallery_id=gallery_m.id))
+
+
+def get_post(job, gallery_m):
+    """Get post from job and gallery_model."""
+    for img_url , img_metadata in job.images:
+        post_url_m, _ = Url.get_or_create(value=img_url)
+        post_m, _ = Post.get_or_create(gallery=gallery_m, url=post_url_m)
+        post_metadata_m, _ = PostMetadata.get_or_create(post=post_m)
+        try:
+            post_metadata_m.metadata.update(img_metadata)
+        except AttributeError:
+            post_metadata_m.metadata = img_metadata
+        post_metadata_m.save()
+        yield post_m
+
+
+@app.route('/g/', defaults={'page': 1, "gallery_id": None})
+@app.route('/g/page/<int:page>', defaults={'gallery_id': None})
+@app.route('/g/<int:gallery_id>', defaults={'page': 1})
+@app.route('/g/<int:gallery_id>/page/<int:page>')
+def gallery(gallery_id=None, page=1):
+    """Get gallery."""
+    gallery_m = models.Gallery.get(id=gallery_id)
+    posts_q = Post.select().where(models.Post.gallery == gallery_m)
+    if posts_q.exists():
+        return render_template(
+            'gallery.html', entries=posts_q, url_input=gallery_m.url.value)
     try:
-        entries = get_or_create_gallery(url_input)
-        return render_template('index.html', entries=entries, url_input=url_input)
-    except NoExtractorError as e:
-        return render_template('index.html', error="No Extractor found.", url_input=url_input)
+        job = CustomJob(gallery_m.url.value)
+        job.run()
+        try:
+            gallery_m.metadata.update(job.gallery_data)
+        except AttributeError:
+            gallery_m.metadata = job.gallery_data
+        gallery_m.save()
+        entries = get_post(job=job, gallery_m=gallery_m)
+        return render_template(
+            'gallery.html', entries=entries, url_input=gallery_m.url.value)
+    except NoExtractorError:
+        flash(
+            'No Extractor found for {}.'.format(gallery_m.url.value), 'error')
+        return redirect(url_for('index'))
 
 
-@app.route('/gallery')
-def gallery():
-    data_json = request.args.get('data', None)
-    data = json.loads(data_json)
-    return render_template('gallery.html', data=data)
+@app.route('/p')
+def gallery_post():
+    """Get gallery post."""
+    no_post_msg = 'No post found with that url.'
+    url_input = request.args.get('u', None)
+    post_url_m, is_created = models.get_or_create(url=url_input)
+    if is_created:
+        flash(no_post_msg, 'error')
+        return redirect(url_for('index'))
+    post_q = models.Post.select().where(models.Post.url == post_url_m)
+    if not post_q.exists():
+        flash(no_post_msg, 'error')
+        return redirect(url_for('index'))
+    return render_template('post.html', entries=post_q)
 
 
 def main():
-    app.run()
+    """Get main function."""
+    init_db()
+    app_run_kwargs = {'debug': True}
+    app.run(**app_run_kwargs)
 
 
 if __name__ == '__main__':

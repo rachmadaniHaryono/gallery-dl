1e6014228d299570241f83a54e153c321fac1721
rachmadaniHaryono <foreturiga@gmail.com>
2017-08-17 21:40:52 +0800

    new: dev: resized image url and url check method

--- gallery_dl/models.py
+++ gallery_dl/models.py
@@ -36,6 +36,20 @@ class Post(BaseModel):
     gallery = ForeignKeyField(Gallery)
     url = ForeignKeyField(Url)
 
+    def get_resized_img_url(self):
+        """Get resized url."""
+        if self.url.value.startswith(('javascript:', '../')):
+            return self.url.value
+        noscheme_url = self.url.value.split('://', 1)[1]
+        return 'https://i.scaley.io/300-border-bg-white/{}'.format(
+            noscheme_url)
+
+    def is_video(self):
+        """Return True if it url is video url."""
+        if self.url.value.endswith('.mp4'):
+            return True
+        return False
+
 
 class PostMetadata(BaseModel):
     """Post."""

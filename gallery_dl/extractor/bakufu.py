fe64ef99287a7dac64fc556e246803e9bcfd5a0d
Rachmadani Haryono <foreturiga@gmail.com>
2021-11-24 10:26:26 +0800

    chg: dev: bakufu, use GalleryExtractor

--- gallery_dl/extractor/bakufu.py
+++ gallery_dl/extractor/bakufu.py
@@ -2,11 +2,13 @@
 # -*- coding: utf-8 -*-
 import collections
 import itertools
+import os
 import re
 import typing as T
 from urllib import parse
 
-from .common import Extractor, Message, get_soup
+from .. import text
+from .common import Extractor, GalleryExtractor, Message, get_soup
 
 BASE_PATTERN = r"(?:https?://)?bakufu.jp"
 
@@ -38,13 +40,18 @@ class BakufuWpContentsExtractor(BakufuExtractor):
             yield Message.Url, new_url, {}
 
 
-class BakufuArchivesExtractor(BakufuExtractor):
-    subcategory = "archives"
-    pattern = BASE_PATTERN + r"/archives/\d+"
+class BakufuGalleryExtractor(BakufuExtractor, GalleryExtractor):
+    pattern = BASE_PATTERN + r"(/archives/\d+)"
+    root = "http://bakufu.jp"
 
     def items(self):
         try:
-            soup = get_soup(self.request(self.url).content)
+            yield from super().items()
+            soup = (
+                self.soup
+                if hasattr(self, "soup")
+                else get_soup(self.request(self.url).content)
+            )
             img_src = {
                 src.strip()
                 for x in soup.find_all("img")
@@ -55,23 +62,10 @@ class BakufuArchivesExtractor(BakufuExtractor):
                 for x in soup.find_all("a")
                 if (href := x.get("href")) and href.strip()
             }
-            data = collections.defaultdict(list)
-            for html_tag in soup.select("footer.entry-meta a"):
-                data["category_"].append(html_tag.text)
-            if html_tag := soup.select_one("h1.entry-title"):
-                data["title"].append(html_tag.text.strip())
-            main_img_urls = []
-            for img_tag in soup.select("div.entry-content a img"):
-                if img_tag.parent:
-                    img_tag_parent_href = img_tag.parent.get("href")
-                    main_img_urls.append(img_tag_parent_href)
-                    yield Message.Url, img_tag_parent_href, data
             s_url_netloc = parse.urlparse(self.url).netloc
             list_product = lambda args: list(itertools.product(*args))
             for url, msg in [  # type: ignore
-                *list_product(
-                    ([x for x in a_href if x not in main_img_urls], [Message.Queue])
-                ),
+                *list_product((a_href, [Message.Queue])),
                 *list_product((img_src, [Message.Url])),
             ]:
                 if url.startswith("//"):
@@ -94,6 +88,7 @@ class BakufuArchivesExtractor(BakufuExtractor):
                         and any(re.match(x, url) for x in self.exclude_external_regex),
                         "exclude regex,%s",
                     ),
+                    (lambda: url in self.main_img_urls, "match main img urls,%s"),
                 ]:
                     if cond():
                         self.log.debug(dbg_msg, url)
@@ -112,8 +107,53 @@ class BakufuArchivesExtractor(BakufuExtractor):
                     and (new_url := replace_url(url))
                     and new_url != url
                 ):
-                    yield Message.Url, new_url, {}
+                    if new_url in self.main_img_urls:
+                        self.log.debug("new url match main img urls,%s", url)
+                    else:
+                        yield Message.Url, new_url, {}
                 else:
                     yield msg, url, {}
         except Exception as err:
             raise err
+
+    def metadata(self, page: str) -> T.Dict[str, T.Any]:
+        """Return a dict with general metadata"""
+        soup = get_soup(page)
+        if not hasattr(self, "soup"):
+            self.soup = soup
+        soup = self.soup
+        data = collections.defaultdict(list)
+        for html_tag in soup.select("footer.entry-meta a"):
+            data["category_"].append(html_tag.text)
+        if html_tag := soup.select_one("h1.entry-title"):
+            data["title"].append(html_tag.text.strip())
+        return data
+
+    def images(self, page: str):
+        soup = get_soup(page)
+        if not hasattr(self, "soup"):
+            self.soup = soup
+        main_img_urls = []
+
+        def update_data(data, url):
+            basename = os.path.splitext(os.path.basename(parse.urlparse(url).path))[0]
+            if match := re.match(r".*_(\d+)(-scaled)?", basename):
+                data["num"] = int(match.group(1))
+            return data
+
+        for img_tag in soup.select("div.entry-content a img"):
+            if (
+                img_tag.parent
+                and (href := img_tag.parent.get("href"))
+                and href not in main_img_urls
+            ):
+                if isinstance(href, list):
+                    self.log.warning("unexpected list, %s", href)
+                    continue
+                main_img_urls.append(href)
+                if new_url := replace_url(href):
+                    main_img_urls.append(new_url)
+                    yield new_url, update_data(text.nameext_from_url(new_url), new_url)
+                else:
+                    yield href, update_data(text.nameext_from_url(href), href)
+        self.main_img_urls = main_img_urls
